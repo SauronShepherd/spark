9dd91b4f6c051af20a7e4197cfd4bb21c32e32bb
__angel__ <angel.alvarez.pascua@gmail.com>
2026-09-04 17:27:34 +0000

    [SPARK-51264][SQL] Close driver connection before JDBC write

--- sql/core/src/test/scala/org/apache/spark/sql/jdbc/JDBCWriteSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/jdbc/JDBCWriteSuite.scala
@@ -17,7 +17,8 @@
 
 package org.apache.spark.sql.jdbc
 
-import java.sql.{Date, DriverManager, Timestamp}
+import java.lang.reflect.{InvocationTargetException, Proxy}
+import java.sql.{Connection, Date, DriverManager, Timestamp}
 import java.time.{Instant, LocalDate}
 import java.util.Properties
 
@@ -26,7 +27,7 @@ import scala.jdk.CollectionConverters._
 
 import org.scalatest.BeforeAndAfter
 
-import org.apache.spark.SparkException
+import org.apache.spark.{SparkException, TaskContext}
 import org.apache.spark.scheduler.{SparkListener, SparkListenerTaskEnd}
 import org.apache.spark.sql.{AnalysisException, DataFrame, Row, SaveMode}
 import org.apache.spark.sql.catalyst.parser.ParseException
@@ -36,6 +37,41 @@ import org.apache.spark.sql.test.SharedSparkSession
 import org.apache.spark.sql.types._
 import org.apache.spark.util.ArrayImplicits._
 import org.apache.spark.util.Utils
+
+object JDBCWriteSuite {
+  @volatile var driverConnectionClosed = false
+
+  def trackingDialect(testUrl: String): JdbcDialect = new JdbcDialect {
+    override def canHandle(jdbcUrl: String): Boolean = jdbcUrl == testUrl
+
+    override def isCascadingTruncateTable(): Option[Boolean] = Some(false)
+
+    override def createConnectionFactory(options: JDBCOptions): Int => Connection = { _ =>
+      if (TaskContext.get() != null) {
+        assert(driverConnectionClosed,
+          "driver JDBC connection should be closed before the distributed write starts")
+        DriverManager.getConnection(options.url)
+      } else {
+        val connection = DriverManager.getConnection(options.url)
+        Proxy.newProxyInstance(
+          connection.getClass.getClassLoader,
+          Array(classOf[Connection]),
+          (_, method, args) => {
+            val methodArgs = if (args == null) Array.empty[AnyRef] else args
+            try {
+              val result = method.invoke(connection, methodArgs: _*)
+              if (method.getName == "close") {
+                driverConnectionClosed = true
+              }
+              result
+            } catch {
+              case e: InvocationTargetException => throw e.getCause
+            }
+          }).asInstanceOf[Connection]
+      }
+    }
+  }
+}
 
 class JDBCWriteSuite extends SharedSparkSession with BeforeAndAfter {
 
@@ -236,6 +272,65 @@ class JDBCWriteSuite extends SharedSparkSession with BeforeAndAfter {
     } finally {
       JdbcDialects.unregisterDialect(testH2Dialect)
       JdbcDialects.registerDialect(H2Dialect())
+    }
+  }
+
+  test("SPARK-51264: close driver connection before starting JDBC write") {
+    val trackingDialect = JDBCWriteSuite.trackingDialect(url)
+    val df = spark.createDataFrame(sparkContext.parallelize(arr1x2), schema2)
+
+    def resetTable(table: String): Unit = {
+      val statement = conn.createStatement()
+      try {
+        statement.executeUpdate(s"DROP TABLE IF EXISTS $table")
+        statement.executeUpdate(s"CREATE TABLE $table (name TEXT(32), id INTEGER)")
+      } finally {
+        statement.close()
+      }
+    }
+
+    def writeAndCheck(table: String, mode: SaveMode, truncate: Boolean = false): Unit = {
+      JDBCWriteSuite.driverConnectionClosed = false
+      val writer = df.write.mode(mode)
+      if (truncate) {
+        writer.option("truncate", true)
+      }
+      writer.jdbc(url, table, new Properties())
+      assert(JDBCWriteSuite.driverConnectionClosed)
+    }
+
+    JdbcDialects.unregisterDialect(H2Dialect())
+    JdbcDialects.registerDialect(trackingDialect)
+    try {
+      resetTable("TEST.SPARK51264_APPEND")
+      writeAndCheck("TEST.SPARK51264_APPEND", SaveMode.Append)
+
+      resetTable("TEST.SPARK51264_TRUNCATE")
+      writeAndCheck("TEST.SPARK51264_TRUNCATE", SaveMode.Overwrite, truncate = true)
+
+      resetTable("TEST.SPARK51264_OVERWRITE")
+      writeAndCheck("TEST.SPARK51264_OVERWRITE", SaveMode.Overwrite)
+
+      val statement = conn.createStatement()
+      try {
+        statement.executeUpdate("DROP TABLE IF EXISTS TEST.SPARK51264_CREATE")
+      } finally {
+        statement.close()
+      }
+      writeAndCheck("TEST.SPARK51264_CREATE", SaveMode.Append)
+    } finally {
+      try {
+        val statement = conn.createStatement()
+        try {
+          statement.executeUpdate("DROP ALL OBJECTS")
+        } finally {
+          statement.close()
+        }
+      } finally {
+        JDBCWriteSuite.driverConnectionClosed = false
+        JdbcDialects.unregisterDialect(trackingDialect)
+        JdbcDialects.registerDialect(H2Dialect())
+      }
     }
   }
 
